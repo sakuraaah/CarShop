28ab009ae0728f05f5a6b0ab77fcccc499e59d7c
sakuraaah <100445166+sakuraaah@users.noreply.github.com>
2023-12-09 18:02:51 +0200

    succesfully connected with backend

--- ClientApp/src/pages/NewProductPage.js
+++ ClientApp/src/pages/NewProductPage.js
@@ -1,12 +1,57 @@
 import React from 'react';
-import { Button, Flex } from 'antd';
+import { useParams } from 'react-router-dom';
+import { Flex, Form as AntdForm } from 'antd';
+import { Button, Form, SideBySide, Input } from '../ui';
+import useQueryApiClient from '../utils/useQueryApiClient';
 
-export const NewProductPage = () => (
-  <Flex gap="small" wrap="wrap">
-    <Button type="primary">Primary Button</Button>
-    <Button>Default Button</Button>
-    <Button type="dashed">Dashed Button</Button>
-    <Button type="text">Text Button</Button>
-    <Button type="link">Link Button</Button>
-  </Flex>
-);
+export const NewProductPage = () => {
+  const[form] = AntdForm.useForm();
+  const { id } = useParams();
+
+  const onSubmit = (updateStatus, status = 'Confirmed') => {
+    form.validateFields()
+      .then((values) => {
+
+        if (!id) {
+          createPost(values);
+        }
+      })
+      .catch((errorInfo) => {
+        form.scrollToField(errorInfo.errorFields[0]?.name, { behavior: 'smooth', block: 'center', scrollMode: 'if-needed' })
+      });
+  }
+
+  const { appendData: createPost, isLoading: createLoading } = useQueryApiClient({
+    request: {
+      url: `api/posts`,
+      method: 'POST'
+    }
+  });
+
+  return (
+    <Form form={form} >
+      <SideBySide
+        left={
+          <>
+            <Input
+              name="header"
+              label={'Header'}
+              rules={[{ required: true }]}
+            />
+            <Input
+              name="text"
+              label={'Text'}
+              rules={[{ required: true }]}
+            />
+          </>
+        }
+      />
+      <Button 
+        htmlType="submit" 
+        onClick={() => onSubmit(false)} 
+        type="primary" 
+        label={'Save'} 
+      />
+    </Form>
+  )
+}
--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -4,8 +4,9 @@ const { env } = require('process');
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:36073';
 
-const context =  [
+const context =  [  // TODO
   "/weatherforecast",
+  "/api/posts",
   "/_configuration",
   "/.well-known",
   "/Identity",
