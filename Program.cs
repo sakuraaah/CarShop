ff37f07a9d60a92c11ee0398451c917ee2e426a5
sakuraaah <100445166+sakuraaah@users.noreply.github.com>
2023-12-12 23:06:28 +0200

    added user roles

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace CarShop
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
