aabc380fd65b76aa607fb428f2aac3afb076a0d1
sakuraaah <100445166+sakuraaah@users.noreply.github.com>
2023-12-19 22:35:05 +0200

    Added migration

---
+++ Migrations/20231219203029_imageFields.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarShop.Migrations
+{
+    public partial class imageFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImgSrc",
+                table: "RentSubmissions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImgSrc",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImgSrc",
+                table: "RentSubmissions");
+
+            migrationBuilder.DropColumn(
+                name: "ImgSrc",
+                table: "AspNetUsers");
+        }
+    }
+}
