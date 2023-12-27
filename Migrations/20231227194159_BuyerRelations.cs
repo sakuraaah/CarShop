c516545c67cf54608efc42cd33163a238f89ec61
sakuraaah <100445166+sakuraaah@users.noreply.github.com>
2023-12-27 22:16:59 +0200

    added buy item migrations

---
+++ Migrations/20231227194159_BuyerRelations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarShop.Migrations
+{
+    public partial class BuyerRelations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BuyerId",
+                table: "BuyItems",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BuyerId",
+                table: "BuyItems");
+        }
+    }
+}
